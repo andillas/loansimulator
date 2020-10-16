c58aa98528c229acaaa5cd4405d184ba34cec3ee
andillas <andillas@yahoo.es>
2020-10-16 11:11:18 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -56,7 +56,7 @@ function initPage(): void {
     elGrid.setPropertyById("name", "title", "Fee Name");
     elGrid.setPropertyById("name", "align", "right");
     elGrid.setPropertyById("name", "display", "none");
-    elGrid.setPropertyById("value", "title", "Amoooount");
+    elGrid.setPropertyById("value", "title", "Amount");
   });
 }
 $(initPage);
